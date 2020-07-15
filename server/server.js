ec6de46fe1a11505b63c16688f0ed7e41bd9b5c0
fai-songprasit <prapawan.songprasit@gmail.com>
2020-07-16 11:39:24 +1200

    add routes to server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const path = require('path')
 const express = require('express')
+const authRoutes = require('./routes/auth')
+const users = require('./routes/api/users')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+server.use('/api/v1', authRoutes)
+server.use('/api/v1/users', users)
 
 module.exports = server
