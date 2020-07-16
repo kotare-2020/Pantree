d0e080586e72d75d459fc0fe431012231af51e53
benjamin-means <ben.w.means@gmail.com>
2020-07-17 10:09:12 +1200

    db functions/routes

--- server/routes/plans.js
+++ server/routes/plans.js
@@ -17,5 +17,32 @@ router.get("/:id", (req, res) => {
   })
 })
 
+router.post("/", (req, res)=>{
+  const plan = req.body
+  const userId = req.body.id
+  db.createPlan(userId, plan)
+  .then(plan=>{
+   res.json(plan[0]) 
+  })
+  .catch(err => {
+    res.status(500).send( "it broke :/" )
+    console.log(err)
+})
+})
+
+
+router.patch("/:id", (req, res)=>{
+  const id= req.params.id
+  const plan = req.body
+  db.editPlan(id, plan)
+  .then( thing =>{
+  res.json(thing)
+})
+  .catch(err => {
+    res.status(500).send( "it broke :/" )
+    console.log(err)
+})
+})
+
 
 module.exports = router
