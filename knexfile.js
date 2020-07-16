b5c370c52242736959e30bcc96437de143137ed2
Joe Butler <joefish.b@gmail.com>
2020-07-16 16:54:44 +1200

    Update knexfile.js with pg config

--- knexfile.js
+++ knexfile.js
@@ -1,45 +1,32 @@
-// Update with your config settings.
-
 module.exports = {
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:',
+    },
+    seeds: {
+      directory: './tests/seeds',
+    },
+    useNullAsDefault: true,
+  },
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './dev.sqlite3',
     },
-    useNullAsDefault: true
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    useNullAsDefault: true,
   },
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+      tableName: 'knex_migrations',
+    },
+  },
+}
