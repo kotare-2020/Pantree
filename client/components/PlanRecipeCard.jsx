dc608d43bbf71e31bf23b8db3f34a5e4d63bc854
Joe Butler <joefish.b@gmail.com>
2020-07-18 17:08:22 +1200

    Update Plan and PlanRecipeCard halfway

--- client/components/PlanRecipeCard.jsx
+++ client/components/PlanRecipeCard.jsx
@@ -3,10 +3,6 @@ import { connect } from 'react-redux'
 import { removeDayRecipe } from '../actions/plan'
 
 class PlanRecipeCard extends React.Component {
-  componentDidMount() {
-    console.log(this.props.uniqueRecipeId)
-  }
-
   handleClick = e => {
     this.props.dispatch(
       removeDayRecipe(this.props.uniqueRecipeId, this.props.dayNumber)
