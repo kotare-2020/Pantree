00f1bfcb1465087ebfefb5da97c57077235e6114
fai-songprasit <prapawan.songprasit@gmail.com>
2020-07-20 10:32:17 +1200

    create action, recuder and api for add add recipe

--- client/apis/recipes.js
+++ client/apis/recipes.js
@@ -1,6 +1,14 @@
 import request from 'superagent'
 
+const recipeURL = '/api/v1/recipes/'
+
 export function getRecipes() {
-    return request.get('/api/v1/recipes/')
+    return request.get(recipeURL)
+        .then(response => response.body)
+}
+
+export function addRecipe(recipe) {
+    return request.post(recipeURL)
+        .send(recipe)
         .then(response => response.body)
 }
