3abfa1b3d64b87c0fe7c578610a301034f0a18b2
student <student@eda-bc-02.localdomain>
2020-07-20 19:18:12 +1200

    change arrows

--- client/components/PlanRecipeCard.jsx
+++ client/components/PlanRecipeCard.jsx
@@ -24,47 +24,48 @@ class PlanRecipeCard extends React.Component {
     )
   }
 
-    handleClone = e => {
-      const selectedDayNumber = this.props.dayNumber
-      const currentDayColumn = this.props.days.find(day => day.dayNumber === selectedDayNumber)
-      const recipeBeingClonedUuid = this.props.recipe.recipeUuid
-      this.props.dispatch(cloneDayRecipe(currentDayColumn, recipeBeingClonedUuid))
-    }
+  handleClone = e => {
+    const selectedDayNumber = this.props.dayNumber
+    const currentDayColumn = this.props.days.find(day => day.dayNumber === selectedDayNumber)
+    const recipeBeingClonedUuid = this.props.recipe.recipeUuid
+    this.props.dispatch(cloneDayRecipe(currentDayColumn, recipeBeingClonedUuid))
+  }
 
-    render() {
-      return (
-        <>
-          <div className="card">
-            <div className="delete-container">
-              <i className=" delete material-icons clickable-icon" onClick={this.handleClick}>delete</i>
-            </div>
-            <i onClick={this.handleClone} className="tiny material-icons clickable-icon">content_copy</i>
-            <span className="card-title">{this.props.recipe.recipeName}</span>
+  render() {
+    return (
+      <>
+        <div className="card small sticky-action card-container">
+          <div className="delete-container">
+            <i className=" delete material-icons clickable-icon" onClick={this.handleClick}>delete</i>
+          </div>
+          <i onClick={this.handleClone} className="tiny material-icons clickable-icon">content_copy</i>
+          <span className="card-title">{this.props.recipe.recipeName}</span>
 
-            <div className="container-arrow-up-down">
-
-
-              <div className=""><i className="tiny material-icons clickable-icon" onClick={this.handleClickUp}>keyboard_arrow_up</i>
-              </div>
-
-              <div className=""><i className="tiny material-icons clickable-icon " onClick={this.handleClickDown}>keyboard_arrow_down</i></div>
-
-            </div>
+          <div className="arrow-container card-action">
 
 
             <div className="container-arrow-left-right">
-              <div className=""><i onClick={this.handleClickUp} className="tiny material-icons clickable-icon">keyboard_arrow_left</i></div>
+              <i onClick={this.handleClickUp} className="tiny material-icons clickable-icon">keyboard_arrow_left</i>
 
-              <div className=""><i className="tiny material-icons clickable-icon" onClick={this.handleClickDown}>keyboard_arrow_right</i>
-              </div>
+              <i className="tiny material-icons clickable-icon" onClick={this.handleClickDown}>keyboard_arrow_right</i>
+              
             </div>
+
+            <div className="container-arrow-up-down">
+              <i className="tiny material-icons clickable-icon" onClick={this.handleClickUp}>keyboard_arrow_up</i>
+              <i className="tiny material-icons clickable-icon " onClick={this.handleClickDown}>keyboard_arrow_down</i>
+            </div>
+
 
           </div>
 
+        </div>
 
-        </>
-      )
-    }
+
+
+      </>
+    )
   }
+}
 
-  export default connect()(PlanRecipeCard)
+export default connect()(PlanRecipeCard)
