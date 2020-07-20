794f9bea356d8123bb578ebcd35e49bcfec463fa
student <student@eda-bc-02.localdomain>
2020-07-20 17:58:22 +1200

    styled arrows

--- client/components/PlanRecipeCard.jsx
+++ client/components/PlanRecipeCard.jsx
@@ -11,38 +11,79 @@ class PlanRecipeCard extends React.Component {
     )
   }
 
-  handleClickDown =()=>{
-   this.props.dispatch(
-     moveRecipeCardDown(this.props.recipe.recipeUuid, this.props.dayNumber)
-   )
+  handleClickDown = () => {
+    this.props.dispatch(
+      moveRecipeCardDown(this.props.recipe.recipeUuid, this.props.dayNumber)
+    )
 
   }
 
-  handleClickUp =()=>{
+  handleClickUp = () => {
     this.props.dispatch(
       moveRecipeCardUp(this.props.recipe.recipeUuid, this.props.dayNumber)
     )
- 
-   }
+
+  }
 
   render() {
     return (
       <>
-        <div className="recipe-card">
-          <h3>{this.props.recipe.recipeName}</h3>
-          <button onClick={this.handleClick}>Remove</button>
-          {/* <button onClick={this.handleClickDown}>Down</button> */}
-          {/* <button onClick={this.handleClickUp}>Up</button> */}
+        <div className="card">
+          <div className="delete-container">
+            <i className=" delete material-icons clickable-icon" onClick={this.handleClick}>delete</i>
+          </div>
+          <span className="card-title">{this.props.recipe.recipeName}</span>
 
-          <a onClick={this.handleClickUp} class="btn-small"><i class="material-icons">keyboard_arrow_up</i></a>
-          <a onClick={this.handleClickDown} class="btn-small"><i class="material-icons">keyboard_arrow_down</i></a>
+          <div className="container-arrow-up-down">
 
-          {/* <i onClick={this.handleClickUp} className="tiny material-icons clickable-icon">keyboard_arrow_up</i>
-          <i onClick={this.handleClickDown} className="tiny material-icons clickable-icon">keyboard_arrow_down</i> */}
+        
+              <div className=""><i className="tiny material-icons clickable-icon" onClick={this.handleClickUp}>keyboard_arrow_up</i>
+            </div>
+     
+            <div className=""><i className="tiny material-icons clickable-icon " onClick={this.handleClickDown}>keyboard_arrow_down</i></div>
+       
+       </div>
 
-          {/* <i onClick={this.handleClone} className="tiny material-icons clickable-icon">content_copy</i> */}
+         
+          <div className="container-arrow-left-right">
+            <div className=""><i onClick={this.handleClickUp} className="tiny material-icons clickable-icon">keyboard_arrow_left</i></div>
+
+            <div className=""><i className="tiny material-icons clickable-icon" onClick={this.handleClickDown}>keyboard_arrow_right</i>
+            </div>
+          </div>
 
         </div>
+  
+
+
+        {/* <div className="card">
+          <div className="delete-container">
+            <i className=" delete material-icons clickable-icon" onClick={this.handleClick}>delete</i>
+          </div>
+          <span className="card-title">{this.props.recipe.recipeName}</span>
+
+          <div className="container">
+
+            <div className="row">
+
+              <div className="col s4 offset-s4"><i className="tiny material-icons clickable-icon" onClick={this.handleClickUp}>arrow_upward</i>
+              </div>
+
+            </div>
+            <div className="row">
+              <div className="col s4"><i onClick={this.handleClickUp} className="tiny material-icons clickable-icon">arrow_back</i></div>
+
+              <div className="col s4 offset-s4"><i className="tiny material-icons clickable-icon" onClick={this.handleClickDown} >arrow_forward</i>
+              </div>
+            </div>
+
+
+            <div className="row">
+              <div className="col s4 offset-s4"><i className="tiny material-icons clickable-icon " onClick={this.handleClickDown}>arrow_downward</i></div>
+            </div>
+          </div>
+
+        </div> */}
 
       </>
     )
