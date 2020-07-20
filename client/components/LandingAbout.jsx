24cda705ea8d76a996a05961b12f2557a14077f8
Joe Butler <joefish.b@gmail.com>
2020-07-20 15:39:04 +1200

    Add favicon

--- client/components/LandingAbout.jsx
+++ client/components/LandingAbout.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LandingAbout = () => {
   return (
     <>
-      <h1>Welcome to Pantree</h1>
+      <img></img>
       <h4>
         A simple meal planner connecting you to locally sourced and sustainable
         produce
