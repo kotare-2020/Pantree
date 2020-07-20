1c28182ce43c7e1ec842e4470688aa6612f1913a
Joe Butler <joefish.b@gmail.com>
2020-07-20 15:27:14 +1200

    Add nav bar image and make Nav bar sticky

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -6,29 +6,32 @@ import { logoutUser } from '../actions/auth'
 const Nav = props => {
   const { logout } = props
   return (
-    <nav>
-      <div className="nav-wrapper green darken-2">
-        <a href="/" className="brand-logo left">
-          Pantree
-        </a>
-        <ul id="nav-mobile" className="right">
-          <li>
-            <NavLink to="/plan">My plan</NavLink>
-          </li>
-          <li>
-            <NavLink to="/shopping-list">Shopping list</NavLink>
-          </li>
-          <li>
-            <NavLink to="/suppliers">Local suppliers</NavLink>
-          </li>
-          <li>
-            <Link to="/" onClick={() => {logout()}}>
-              Log out
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div className="navbar-fixed">
+      <nav>
+        <div className="nav-wrapper green darken-2">
+          <a href="/" className="brand-logo left">
+            <img className="brand-logo left brand-navbar"src="/images/brand/pantree-logo.svg" />
+          </a>
+          
+          <ul id="nav-mobile" className="right">
+            <li>
+              <NavLink to="/plan">My plan</NavLink>
+            </li>
+            <li>
+              <NavLink to="/shopping-list">Shopping list</NavLink>
+            </li>
+            <li>
+              <NavLink to="/suppliers">Local suppliers</NavLink>
+            </li>
+            <li>
+              <Link to="/" onClick={() => {logout()}}>
+                Log out
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   )
 }
 
