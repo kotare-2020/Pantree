0fdc649dfa15984e018c97cce771c8f0b6306218
Joe Butler <joefish.b@gmail.com>
2020-08-03 17:04:47 +1200

    Add PublicRoute

--- client/components/PublicRoute.jsx
+++ client/components/PublicRoute.jsx
@@ -2,23 +2,14 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
 
-import Nav from './Nav'
-
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const PublicRoute = ({ component: Component, ...rest }) => {
   const isAuthenticated = rest.isAuthenticated
 
   return (
     <Route
       {...rest}
       render={props =>
-        isAuthenticated ? (
-          <>
-            <Nav />
-            <Component {...props} />
-          </>
-        ) : (
-          <Redirect to='/' />
-        )
+        isAuthenticated ? <Redirect to='/' /> : <Component {...props} />
       }
     />
   )
@@ -30,4 +21,4 @@ const mapStateToProps = globalState => {
   }
 }
 
-export default connect(mapStateToProps)(PrivateRoute)
+export default connect(mapStateToProps)(PublicRoute)
