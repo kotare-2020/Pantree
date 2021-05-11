c1ba3970adb8ee3b24a8f68ffebcf4f5b0a8ff1d
rose-mead <rose-mead@users.noreply.github.com>
2021-05-12 09:21:44 +1200

    change column to integer

--- migrations/20200716113639_recipes_ingredients.js
+++ migrations/20200716113639_recipes_ingredients.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('recipes_ingredients', (table) => {
         table.increments('id').primary()
-        table.decimal('quantity', 2)
+        table.integer('quantity')
         table.integer('recipe_id')
         table.integer('ingredient_id')
     })
